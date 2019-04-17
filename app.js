07e760968cf24e2f96f2cbcdbb70f37c807a5f78
Valerio <valerioly@gmail.com>
2019-04-17 14:11:37 +0200

    nodejs test non funzionante

--- app.js
+++ app.js
@@ -14,11 +14,55 @@ function login(){
         // Handle Errors here.
         var errorCode = error.code;
         var errorMessage = error.message;
-        
+
         window.alert("Error: " + errorMessage);
     });
 }
 
 function indietro(){
     location.href = "/index.html";
+}
+
+// da sistemare
+
+const nodeMailer = require('nodemailer');
+
+// colleggo elementi 
+var dataPrenota =  document.getElementById("dataPrenota").value;
+var nomePrenota = document.getElementById("nomePrenota").value;
+var cognomePrenota = document.getElementById("cognomePrenota").value;
+var emailPrenota = document.getElementById("emailPrenota").value;
+var nPersone = document.getElementById("personePrenota").value;
+var notePrenota = document.getElementById("notePrenota").value;
+
+
+
+exports.sendMail = function(req,res){
+    const transporter = nodeMailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,  
+        auth: {
+            user: 'InfluencerOutreachExperts@gmail.com',
+            pass: 'vale5398'
+        }
+    });
+    const mailOptions = {
+        from: emailPrenota, // sender address
+        to: 'InfluencerOutreachExperts@gmail.com', // list of receivers
+        subject: "Prenotazione Tavolo", // Subject line
+        text: "Data"+dataPrenota+"Nome"+nomePrenota+"Cognome"+cognomePrenota+"emailPrenota"+emailPrenota+"Numero Persone"+nPersone+"notePrenota"+notePrenota, // plain text body
+        html: '<b>Prenotazione Tavolo</b>' // html body
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(400).send({success: false})
+        } else {
+            res.status(200).send({success: true});
+        }
+    });
+
+
 }
